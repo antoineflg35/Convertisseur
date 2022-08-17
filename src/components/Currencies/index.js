032e4765de1ac0b6bdcca29aefc4993a66969c9d
antoineflg35 <antoineflegeau35@gmail.com>
2022-08-17 10:43:14 +0200

    Mise en place du SCSS

--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -4,7 +4,7 @@ import Currency from './Currency';
 function Currencies() {
   return (
     <main className="currencies">
-      <h2 className="currencies__title">currencies</h2>
+      <h2 className="currencies__title">Currencies</h2>
       <ul className="currencies__list">
         <Currency />
         <Currency />
