51e3454018d846f4af2e52a018fcbd32e10efe5e
antoineflg35 <antoineflegeau35@gmail.com>
2022-08-17 13:56:15 +0200

    Transformation du composant Converter en classe - Placement dans le state de la variable d'état open qui permet de déterminer si la liste des devises est affichée ou non

--- src/components/ConvertedAmount/index.js
+++ src/components/ConvertedAmount/index.js
@@ -1,12 +1,18 @@
+import PropTypes from 'prop-types';
 import './styles.scss';
 
-function ConvertedAmount() {
+function ConvertedAmount({amount, currency}) {
   return (
     <footer className="converted-amount">
-      <p className="converted-amount__amount">1.09</p>
-      <p className="converted-amount__currency">United States Dollar</p>
+      <p className="converted-amount__amount">{ amount }</p>
+      <p className="converted-amount__currency">{ currency }</p>
     </footer>
   );
 }
+
+ConvertedAmount.propTypes = {
+  amount: PropTypes.number.isRequired,
+  currency: PropTypes.string.isRequired,
+};
 
 export default ConvertedAmount;
--- src/components/Converter/index.js
+++ src/components/Converter/index.js
@@ -1,18 +1,32 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable no-useless-constructor */
 import './styles.scss';
-
+import React from 'react';
 import currenciesData from 'src/data/currencies';
 import BaseAmount from '../BaseAmount';
 import ConvertedAmount from '../ConvertedAmount';
 import Currencies from '../Currencies';
 
-function Converter() {
-  return (
-    <div className="converter">
-      <BaseAmount amount={2} />
-      <Currencies currencies={currenciesData} />
-      <ConvertedAmount />
-    </div>
-  );
+class Converter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: true,
+    };
+  }
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <div className="converter">
+        <BaseAmount amount={2} />
+        { open && <Currencies currencies={currenciesData} /> }
+        <ConvertedAmount amount={1.08} currency="USA" />
+      </div>
+    );
+  }
 }
 
 export default Converter;
