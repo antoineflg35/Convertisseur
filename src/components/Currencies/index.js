de5720943c11f4e693db77b478a73ad820538ed3
antoineflg35 <antoineflegeau35@gmail.com>
2022-08-17 09:46:12 +0200

    Mise en place de la structure du composant Currencies

--- src/components/Currencies/index.js
+++ src/components/Currencies/index.js
@@ -1,7 +1,15 @@
 import './styles.scss';
 
 function Currencies() {
-  return <div>Composant Currencies</div>;
+  return (
+    <main className="currencies">
+      <h2 className="currencies__title">currencies</h2>
+      <ul className="currencies__list">
+        <li className="currencies__item">United State Dollar</li>
+        <li className="currencies__item">United State Dollar</li>
+      </ul>
+    </main>
+  );
 }
 
 export default Currencies;
